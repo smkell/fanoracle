887d054d3369841c089b243d0a2732c275bcfbf1
Sean Kell <sean.michael.kell@gmail.com>
2015-05-08 09:37:04 -0400

    Move message parsing to new module.
    Added functions to the message module for parsing the HTTP version
    field of the status line.

--- src/http/lib.rs
+++ src/http/lib.rs
@@ -1,13 +1,16 @@
 //! An implemention of the HTTP protocol for both clients and servers.
 
 #![feature(convert)]
+#![feature(slice_patterns)]
 
 mod statuscodes;
+mod message;
 pub mod client;
 
 pub use statuscodes::*;
 
 #[derive(PartialEq, Debug)]
 pub enum HttpVersion {
+    Version10,
     Version11,
 }
