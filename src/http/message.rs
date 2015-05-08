15eb7781c7b9eb170d48fad2df0b01f6abe8021f
Sean Kell <sean.michael.kell@gmail.com>
2015-05-08 12:12:03 -0400

    Refactor message parser to operate on chars.
    This makes the code more readable, and shouldn't affect performance
    significantly (as char and u8 have the exact same size and will
    should be compiled to the same code).

--- src/http/message.rs
+++ src/http/message.rs
@@ -2,10 +2,9 @@
 
 use super::HttpVersion;
 
-fn parse_httpname(msg: &[u8]) -> Option<&[u8]> {
+fn parse_httpname(msg: &[char]) -> Option<&[char]> {
 	match msg {
-		[0x48, 0x54, 0x54, 0x50, rest..] => {
-			println!("Found the HTTP name! Rest: {:?}", rest);
+		['H', 'T', 'T', 'P', rest..] => {
 			Some(rest)
 		},
 		_ => None
@@ -17,16 +16,15 @@ fn parse_httpname(msg: &[u8]) -> Option<&[u8]> {
 /// # Return
 /// If parsing was successful the return value is a tuple of the parsed HttpVersion, and a slice
 /// containing any remaining bytes from the message.
-fn parse_httpversion(msg: &[u8]) -> Option<(HttpVersion, &[u8])> {
+fn parse_httpversion(msg: &[char]) -> Option<(HttpVersion, &[char])> {
 
 	// Parse out the http name.
 	match parse_httpname(msg) {
 		Some(remain) => match remain {
-			[0x2f, major, 0x2E, minor, rest..] => {
-				println!("Matched /major.minor! major: {:?} minor: {:?} rest: {:?}", major, minor, rest);
+			['/', major, '.', minor, rest..] => {
 				match (major, minor) {
-					(0x31, 0x30) => Some((HttpVersion::Version10, rest)),
-					(0x31, 0x31) => Some((HttpVersion::Version11, rest)),
+					('1', '0') => Some((HttpVersion::Version10, rest)),
+					('1', '1') => Some((HttpVersion::Version11, rest)),
 					_ => None
 				}
 			},
@@ -39,10 +37,11 @@ fn parse_httpversion(msg: &[u8]) -> Option<(HttpVersion, &[u8])> {
 #[test]
 fn parse_httpversion_test() {
 	use super::HttpVersion;
+    use std::iter::FromIterator;
 
 	let test_cases = vec![
-		(vec![0x48,0x54,0x54,0x50,0x2F,0x31,0x2E,0x31], (HttpVersion::Version11, 0)),
-		(vec![0x48,0x54,0x54,0x50,0x2F,0x31,0x2E,0x30], (HttpVersion::Version10, 0)),
+        (Vec::from_iter("HTTP/1.1".chars()), (HttpVersion::Version11, 0)),
+        (Vec::from_iter("HTTP/1.0".chars()), (HttpVersion::Version10, 0)),
 	];
 
 	for (msg, (expect_msg, expect_len)) in test_cases {
